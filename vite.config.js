dff8832c905ecb5a547cac9944c918cdd41a4d41
mien <2502mien@gmail.com>
2023-03-15 11:46:56 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/test_github_page/' // Thay tên repository của các bạn vào đây nhé
+      : '/',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
