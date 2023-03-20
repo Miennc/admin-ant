e191b5971a66a54e4e3d93fdc3fe3a3ddf6aebce
mien <2502mien@gmail.com>
2023-03-20 09:26:10 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/admin-ant/' // Thay tên repository của các bạn vào đây nhé
-      : '/',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
